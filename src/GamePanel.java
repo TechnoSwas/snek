71c15960253d834fae959f444867cc62189aa7bf
TechnoSwas <swas.swastik@hotmail.com>
2026-07-20 10:12:25 +0400

    Added a grid and a scoreboard and a play again key to the game

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -10,10 +10,11 @@ import java.awt.event.KeyEvent;
 public class GamePanel extends JPanel implements ActionListener, KeyListener {
     Snake snake = new Snake();
     Food food = new Food();
+    int gameScore = 0;
     boolean gameOver = false;
     Timer timer = new Timer(150, this);
     public GamePanel() {
-        setBackground(Color.BLACK);
+        setBackground(new Color(170, 215, 81));
         setPreferredSize(new Dimension(500, 500));
         timer.start();
         setFocusable(true);
@@ -25,6 +26,7 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
             if (snake.body.get(0)[0] == food.x && snake.body.get(0)[1] == food.y) {
                 snake.grow();
                 food.respawn();
+                gameScore++;
             }
             if (snake.hitWall() || snake.hitSelf()) {
                 gameOver = true;
@@ -39,15 +41,25 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
         super.paintComponent(g);
         if (gameOver) {
             g.setColor(Color.WHITE);
-            g.drawString("Game Over", 200, 250);
+            g.drawString("Game Over, click R to play again.", 150, 250);
+            g.drawString("Final Score " + gameScore, 200, 270);
             return;
+        }
+        g.setColor(Color.DARK_GRAY);
+        for (int i = 0; i < 500; i += 20) {
+            g.drawLine(i, 0, i, 500);
+        }
+        for (int i = 0; i < 500; i += 20) {
+            g.drawLine(0, i, 500, i);
+
         }
         g.setColor(Color.RED);
         g.fillRect(food.x, food.y, 20, 20);
         for (int i = 0; i < snake.body.size(); i++) {
-              g.setColor(Color.GREEN);
+              g.setColor(new Color(23, 50, 3));
               g.fillRect(snake.body.get(i)[0], snake.body.get(i)[1], 20, 20);
         }
+        g.drawString("Score: " + gameScore, 10,20);
     }
     public void keyTyped(KeyEvent e) {}
     public void keyReleased(KeyEvent e) {}
@@ -66,6 +78,12 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
         if (e.getKeyCode() == KeyEvent.VK_D) {
             snake.dirX = 1;
             snake.dirY = 0;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_R) {
+            snake = new Snake();
+            food = new Food();
+            gameScore = 0;
+            gameOver = false;
         }
     }
 }
